ac99a101f94ed378c4a84ab3115612410c2055db
AgrawalDrishti <agrawal.24@iitj.ac.in>
2024-11-07 21:08:36 +0530

    Initializing Consumer and setting up repeated topic publishing

--- clients/producer.js
+++ clients/producer.js
@@ -4,11 +4,36 @@ const socket = io('http://localhost:3000')
 socket.connect()
 
 socket.emit('addTopic', 'topic1')
+socket.emit('addTopic', 'topic2')
 
-socket.emit('publish' ,'topic1', 'Hello, world!',(err, res)=>{
-    if(err){
-        console.error(err)
-    }else{
-        console.log(res)
+function getRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-})
+    return result;
+}
+
+setInterval(() => {
+    const randomMessage = getRandomString(10);
+    socket.emit('publish', 'topic1', randomMessage, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(res);
+        }
+    });
+}, 5000);
+
+setInterval(() => {
+    const randomMessage = getRandomString(10);
+    socket.emit('publish', 'topic2', randomMessage, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(res);
+        }
+    });
+}, 3000);
